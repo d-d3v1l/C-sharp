b6b63f3f640b0ec26efb0334de62b5a98c625619
d-d3v1l <33029888+d-d3v1l@users.noreply.github.com>
2017-11-15 18:51:41 +0200

    Commit message 1

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "symbol";
+            for (int currentSymbol = 0; currentSymbol < text.Length; currentSymbol++)
+            {
+                Console.WriteLine(currentSymbol);
+            }
+        }   
+    }
+ }
+
+
+ 
+
+ 
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int jumps = int.Parse(Console.ReadLine());
+
+            var solution = jumps % n;
+            Console.WriteLine(solution);
+                // kur bate
+        }
+    }
+}
